5691723ce5da5dcf7a4e65af3b1592660d0a901f
Julian <julianfermani45@gmail.com>
2021-02-22 16:13:41 -0300

    agregue y cambie titulos + diseños

--- js/app.js
+++ js/app.js
@@ -4,57 +4,18 @@ function mostrarDiscord() {
   alert('¡Buscame como Julián#7777!');
 }
 
-// Desplazamiento de sections
-window.sr = ScrollReveal();
+ // TEXTO H1
 
-  sr.reveal('.nav__li', {
-    duration: 2000,
-    origin: 'top',
-    distance: '1000px'
-  });
+ new TypeIt(".main__h1", {
+   strings  : ["¡Hola! Me llamo <span>Julian</span> y"," soy desarrollador web","<span>frontend</span>"],
+   speed: 100,
+   waitUntilVisible: true
+ }).go();
 
-// Main h1
-  sr.reveal('.main__h1', {
-    duration: 2000,
-    origin: 'right',
-    distance: '-1000px',
-  });
+// TEXTO DE PROJECTS
 
-// Ilustracion del main
-  sr.reveal('.main__svg', {
-      duration: 2000,
-      origin: 'left',
-      distance: '-1000px',
-  });
-
-// Section aboutme
-  sr.reveal('.aboutme__contenedor', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '1000px',
-  });
-
-// Filas del section skills
-  sr.reveal('.primerFila', {
-    duration: 1500,
-    origin: 'bottom',
-    distance: '1000px',
-  })
-
-  sr.reveal('.segundaFila', {
-    duration: 2000,
-    origin: 'bottom',
-    distance: '1000px',
-  })
-
-  sr.reveal('.tercerFila', {
-    duration: 2500,
-    origin: 'bottom',
-    distance: '1000px',
-  })
-
-  sr.reveal('.cuartaFila', {
-    duration: 3000,
-    origin: 'bottom',
-    distance: '1000px',
-  })
+const instance = new TypeIt(".projects__ad", {
+  strings: ["Proyectos aún en construcción, disculpe las molestias."],
+  waitUntilVisible: true,
+  speed: 100,
+}).go();
